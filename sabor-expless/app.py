291cefe495b8a46109737278e7f1588c1736124e
Kenzo <kathelinishii@outlook.com>
2024-08-15 15:32:49 -0300

    print

---
+++ sabor-expless/app.py
@@ -0,0 +1,9 @@
+print('Salor Expless\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')   
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opcão: ')
+print('\nVocê escolheu', opcao_escolhida)
